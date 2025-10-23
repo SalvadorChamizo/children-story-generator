5130b94cda88e45c0be1c0fec9b80321f183bf5d
Salvador Chamizo <salvadorchamizo@gmail.com>
2025-10-23 18:33:27 +0200

    feat: Add basic structure

---
+++ src/api.ts
@@ -0,0 +1,8 @@
+const textarea = document.getElementById("textarea");
+
+export async function generateStory {
+    const submitButton = document.getElementById("submit-button");
+
+
+
+}
---
+++ src/router.ts
@@ -0,0 +1,5 @@
+export function router(route: string): string {
+    switch (route) {
+
+    }
+}
