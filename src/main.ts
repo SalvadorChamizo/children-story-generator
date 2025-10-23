31879032a7874fb13516fa5242a1d91d1e752220
Salvador Chamizo <salvadorchamizo@gmail.com>
2025-10-23 19:24:41 +0200

    feat: It works in theory

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,12 @@ import { GoogleGenAI } from "@google/genai";
 import { generateStory } from "./api";
 import { validateTextUntilValid } from "../backend/src/utils/CheckStory"
 
-const ai = new GoogleGenAI({});
-
 async function main() {
     const story = document.getElementById("generated-story")!;
-    const text = await generateStory();
-    story.textContent = await validateTextUntilValid(text);
+    //const text = await generateStory();
+    //story.textContent = await validateTextUntilValid(text);
+    if (story)
+        story.textContent = await generateStory();
 }
 
 await main();
